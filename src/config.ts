e7934a484d5a6e74e32e53cb6812f8e03bea7309
uztbt <yujitabata@icloud.com>
2021-08-29 18:19:55 +0900

    Add Command Buffer

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  socketIOURL: "http://localhost:3000",
+  socketIOOpts: {
+    reconnectionDelayMax: 10000
+  },
+  sendCommandDictInterval: 16
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("index.ts is loaded!")
+import { CommandBuffer } from './UserInput';
+import { SocketIOTransmitter } from './SocketIOTransmitter'
+import { config } from './config';
+
+function main() {
+  console.log("main() is called!");
+  const transmitter = new SocketIOTransmitter(
+    config.socketIOURL,
+    config.socketIOOpts);
+  if (typeof transmitter === "undefined") {
+    throw Error("Failed to initialize transmitter");
+  } else {
+    console.log("transmitter is not undefined")
+  }
+  console.log(`transmitter: ${transmitter}`);
+  const commandBuffer = new CommandBuffer(transmitter); 
+}
+
+main();
